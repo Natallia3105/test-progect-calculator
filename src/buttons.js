a919eb3dec5666c473103b257e797adc7a92fbe6
natalagoncaruk <honcharukna@gmail.com>
2023-11-16 13:45:07 +0200

    update index html and css

---
+++ src/buttons.js
@@ -0,0 +1,23 @@
+/*
+let buttons = {
+  buttons: [
+    { value: "1" },
+    { value: "2" },
+    { value: "3" },
+    { value: "4" },
+    { value: "5" },
+    { value: "6" },
+    { value: "7" },
+    { value: "8" },
+    { value: "9" },
+    { value: "0" },
+    { value: "C" },
+    { value: "+" },
+    { value: "-" },
+    { value: "*" },
+    { value: "/" },
+    { value: "=" },
+  ],
+};
+export default buttons;
+*/
